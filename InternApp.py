f3a0449668a48a92842f7c11641e059e953f1a2b
bingxinnn <143489588+bingxinnn@users.noreply.github.com>
2023-09-23 22:32:56 +0800

    Update InternApp.py

--- InternApp.py
+++ InternApp.py
@@ -92,6 +92,64 @@ def jianyongPortfolio():
 def xinyiPortfolio():
     return render_template('xinyi_portfolio.html')
 
+@app.route("/companyform", methods=['GET', 'POST'])
+def Company():
+    # com_id = request.form['com_id']
+    # com_pwd = request.form['com_pwd']
+    com_name = request.form['com_name']
+    com_address = request.form['com_address']
+    com_HP = request.form['com_hp']
+    com_email = request.form['com_mail']
+    com_description = request.form['com_description']
+    com_website = request.form['com_website']
+    com_logo_file = request.files['company_logo_file']
+    job_title = request.form['job_title']
+    job_type = request.form['job_type']
+    job_description = request.form['job_description']
+    # status = request.form['com_id']
+    
+
+
+    insert_sql = "INSERT INTO company VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    if com_logo_file.filename == "":
+        return "Please select an image file"
+
+    try:
+
+        cursor.execute(insert_sql, (com_name, com_address, com_HP,com_email,com_description,com_website,job_title,job_type,job_description))
+        db_conn.commit()
+        # Uplaod image file in S3 #
+        comp_image_file_name_in_s3 = str(com_name) + "_logo_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=com_image_file_name_in_s3, Body=com_logo_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                com_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('AddComOutput.html', name=com_name)
+        #return to company profile page
+
 @app.route("/searchStudentProcess", methods=['GET', 'POST'])
 def searchStudProcess():
     stud_id = request.form['stud_id']
